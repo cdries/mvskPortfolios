ab8b653bea6387ab1fdc8a6ec33b3f2dd48327b6
Dries Cornilly <driescornilly@gmail.com>
2019-01-31 16:22:57 +0100

    add herindahl and ERC objective functions

--- R/fERC.R
+++ R/fERC.R
@@ -1,8 +1,6 @@
 
 fERC <- function(w, M2) {
 
-  # TODO
-
   ### input
   # w         : weight vector
   # M2        : covariance matrix
@@ -12,13 +10,17 @@ fERC <- function(w, M2) {
   # gradient  : the gradient of the objective with respect to w
 
   # objective value
-  s <- sqrt(diag(M2))
-  wSigw <- sum(w * M2 %*% w)
-  divratio <- sum(w * s) / sqrt(wSigw)
-  obj <- -divratio
+  p <- length(w)
+  Sigw <- M2 %*% w
+  pctRC <- w * Sigw / sum(w * Sigw)
+  obj <- sum((pctRC - 1 / p)^2)
 
   # gradient
-  gr <- -(s / sqrt(wSigw) - sum(w * s) / (wSigw)^1.5 * M2 %*% w)
+  sp <- sqrt(sum(w * Sigw))
+
+  dff <- pctRC - 1 / p
+  gr <- 2 * (sp^2 * (M2 %*% (w * dff) + dff * Sigw) -
+               2 * sum(w * dff * Sigw) * Sigw) / sp^4
 
   return (list("objective" = obj, "gradient" = gr))
 }
---
+++ R/fHI.R
@@ -0,0 +1,19 @@
+
+fHI <- function(w) {
+
+  ### input
+  # w         : weight vector
+  #
+  ### output
+  # objective : standardized Herfindahl index
+  # gradient  : the gradient of the objective with respect to w
+
+  # objective value
+  p <- length(w)
+  obj <- (sum(w^2) - 1 / p) / (1 - 1 / p)
+
+  # gradient
+  gr <- 2 * w / (1 - 1 / p)
+
+  return (list("objective" = obj, "gradient" = gr))
+}
